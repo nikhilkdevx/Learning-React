03bf67df61b9db78f43470418421d93518e17292
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-08-27 11:39:57 +0530

    to do

--- Ludo/src/App.jsx
+++ Ludo/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import LudoBoard from './LudoBoard'
+import ToDo from './ToDo'
 function App() {
   
   return (
     <>
-     <LudoBoard/> 
+     <ToDo/> 
     </>
   )
 }
---
+++ Ludo/src/ToDo.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
+
+export default function ToDo(){
+    let [todos,setToDos] = useState([{task : "task1",id : uuidv4()}]);
+    let [newTodo,setnewTodo] = useState("");
+    let addNewTask = ()=> {
+        setToDos((prevTodos)=>{
+            return [...todos,{task : newTodo,id : uuidv4()}]
+        });
+        setnewTodo("");  
+    }
+    let updateTaskVal = (event) => {
+        setnewTodo(event.target.value);
+    } 
+    let deleteTodo = (id) =>{
+    setToDos((prevTodos)=>todos.filter((prevTodos)=> prevTodos.id != id));
+    
+    }
+    return(
+        <div>
+            <input placeholder="Enter Tasks" 
+            value={newTodo} 
+            onChange={updateTaskVal}
+            ></input>
+            &nbsp;
+            &nbsp;
+            <button onClick={addNewTask}>Add</button>
+            <br></br>
+            <br></br>
+            <br></br>
+            <hr></hr>
+            <h3>Tasks Todo</h3>
+            <ul>
+                {todos.map((todo)=>{
+                    return <li key={todo.id}>
+                        <span>{todo.task}</span> 
+                        &nbsp;
+                        &nbsp;
+                        <button onClick={()=>deleteTodo(todo.id)}>Delete</button>   
+                    </li>
+                })
+                }
+            </ul>
+        </div>
+    )
+}
