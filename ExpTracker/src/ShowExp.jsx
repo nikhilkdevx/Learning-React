f5fee34175660079520d7a97ed3ee230e40d7e15
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-09 13:49:52 +0530

    learnt about filter functionality

--- ExpTracker/src/ShowExp.jsx
+++ ExpTracker/src/ShowExp.jsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import ExpCard from "./ExpCard";
 
 export default function ShowExp({orgData}){
+    const [selectedCategory,setSelectedCategory] = useState("");
+    let handleFilterChange = (event) => {
+        setSelectedCategory(event.target.value);
+    }
+    const filterdData = selectedCategory === "" ? orgData :
+        orgData.filter(expense => expense.category === selectedCategory);
     return (
         <>
             <h5>Filter &nbsp;&nbsp;
-                <select>
+                <select onChange={handleFilterChange}>
                     <option value={""} >By Category</option>
                     <option value={"Food"} >Food</option>
                     <option value={"Travel"} >Travel</option>
-                    <option value={"Shopping"} >Shopping</option>
-                    <option value={"Bills"} >Bills</option>
+                    <option value={"Shopping"}>Shopping</option>
+                    <option value={"Bills"}  >Bills</option>
                 </select>
             </h5>
-           {orgData.map((expense)=>{
+           {filterdData.map((expense)=>{
             return <ExpCard expense= {expense} key = {expense.id}/>
            })}
         </>
