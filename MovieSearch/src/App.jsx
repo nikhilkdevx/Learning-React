0bca538a50e616597a1f5a0d528e039488b9844a
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-13 22:28:32 +0530

    Done Movie Data Retrival

--- MovieSearch/src/App.jsx
+++ MovieSearch/src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import InputBox from './InputBox'
+import MovieCard from './MovieCard';
 
 function App() {
-  
+  const [movieData, setMovieData] = useState({
+    title: "",
+    year: "",
+    imdb: "",
+    actors: "",
+    director: "",
+    released: "",
+    image : "",
+  });
 
   return (
     <>
-      <InputBox/>
+      <InputBox  setMovieData = {setMovieData} />
+      <MovieCard movieData = {movieData} /> 
     </>
   )
 }
--- MovieSearch/src/InputBox.jsx
+++ MovieSearch/src/InputBox.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 
 const API_URL = "https://www.omdbapi.com/";
 const API_KEY = "72fb7137";
-export default function InputBox() {
+
+export default function InputBox({setMovieData}) {
     const [formData, setFormData] = useState("");
     const handleInputChange = ((event) => {
         setFormData(event.target.value);
@@ -12,8 +13,19 @@ export default function InputBox() {
         const movieData =await fetch(`${API_URL}?t=${formData}&apikey=${API_KEY}`);
         const jsonData = await movieData.json();
         console.log(jsonData);
+        const newData = {
+            title : jsonData.Title,
+            year : jsonData.Year,
+            imdb : jsonData.imdbRating,
+            actors : jsonData.Actors,
+            director : jsonData.Director,
+            released : jsonData.Released,
+            image : jsonData.Poster,
+        };
+        console.log(newData);
+        setMovieData(newData);
         setFormData("");
-    })
+    });
     return (
         <>
             <form onSubmit={handleSubmit}>
